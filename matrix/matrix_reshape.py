f1e439a0b0e0100a9ad84c159dbbc78e3637c93f
ruqianq <daisy@acts.net>
2021-08-02 15:25:18 -0400

    add the description and tests

--- matrix/matrix_reshape.py
+++ matrix/matrix_reshape.py
@@ -1,9 +1,29 @@
+# In MATLAB, there is a handy function called reshape which can reshape an m x n matrix into a new one with a different size r x c keeping its original data.
+#
+# You are given an m x n matrix mat and two integers r and c representing the number of rows and the number of columns of the wanted reshaped matrix.
+#
+# The reshaped matrix should be filled with all the elements of the original matrix in the same row-traversing order as they were.
+#
+# If the reshape operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+#
+
 from typing import List
 
 
 def matrix_reshape(mat: List[List[int]], r: int, c: int) -> List[List[int]]:
     m = len(mat)
     n = len(mat[0])
-    if m * n <= r * c and (r != m or c != n):
-        return [[]]
+    staging = []
+    result = []
+    if m * n == r * c and (m, n) != (r, c):
+        for i in range(m):
+            for j in range(n):
+                staging.append(mat[i][j])
+        new_mat_row = []
+        for s in staging:
+            new_mat_row.append(s)
+            if len(new_mat_row) == c:
+                result.append(new_mat_row)
+                new_mat_row = []
+        return result
     return mat
--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -8,6 +8,18 @@ class TestMatrixModule(unittest.TestCase):
         reshaped_matrix = matrix_reshape([[1, 2], [3, 4]], 1, 1)
         self.assertEqual(reshaped_matrix, [[1, 2], [3, 4]])
 
+    def test_matrix_reshape_if_r_c_same_as_origin(self):
+        reshaped_matrix = matrix_reshape([[1, 2, 0], [3, 4, 0]], 2, 3)
+        self.assertEqual(reshaped_matrix, [[1, 2, 0], [3, 4, 0]])
+
+    def test_matrix_reshape(self):
+        reshaped_matrix = matrix_reshape([[1, 2, 0], [3, 4, 0]], 3, 2)
+        self.assertEqual(reshaped_matrix, [[1, 2], [0, 3], [4, 0]])
+
+    def test_matrix_reshape_r_c_bigger_than_m_n(self):
+        reshaped_matrix = matrix_reshape([[1, 2], [3, 4]], 2, 4)
+        self.assertEqual(reshaped_matrix, [[1, 2], [3, 4]])
+
 
 if __name__ == '__main__':
     unittest.main()
