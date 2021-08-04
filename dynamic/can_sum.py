c5242f324a9a8bb8dddcf010f12abe7d9e2ff48f
ruqianq <daisy@acts.net>
2021-08-04 15:18:19 -0400

    add new question

---
+++ dynamic/can_sum.py
@@ -0,0 +1,33 @@
+# You are given an integer array nums and an integer target.
+#
+# You want to build an expression out of nums by adding one of the symbols '+' and '-' before each integer in nums and
+# then concatenate all the integers.
+#
+# For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the
+# expression "+2-1".
+# Return the number of different expressions that you can build, which evaluates to target.
+
+# this is can sum problem to find out all the possible paths (sum) to get to target
+# create a grid with len(nums) * len(nums)
+
+def dp(nums, target, index, curr_sum):
+    memo = {}
+    if (index, curr_sum) in memo:
+        return memo[(index, curr_sum)]
+
+    if index < 0 and curr_sum == target:
+        return 1
+    if index < 0:
+        return 0
+
+    positive = dp(nums, target, index-1, curr_sum + nums[index])
+    negative = dp(nums, target, index-1, curr_sum + -nums[index])
+
+    memo[(index, curr_sum)] = positive + negative
+    return memo[(index, curr_sum)]
+
+
+def find_target_sum_ways(nums, S):
+    index = len(nums) - 1
+    curr_sum = 0
+    return dp(nums, S, index, curr_sum)
--- tests/test_dynamic.py
+++ tests/test_dynamic.py
@@ -1,10 +1,12 @@
 import unittest
 
+from dynamic.can_sum import find_target_sum_ways
+
 
 class DynamicTest(unittest.TestCase):
     def test_min_path_sum(self):
-
-        self.assertEqual(True, False)
+        result = find_target_sum_ways([1,1,1,1,1], 3)
+        self.assertEqual(result, 5)
 
 
 if __name__ == '__main__':
