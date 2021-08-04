1b4ee0941259fea1c4e83185e2ab8798ad9cf061
ruqianq <daisy@acts.net>
2021-08-04 17:01:38 -0400

    add the new question

---
+++ dynamic/counting_bit.py
@@ -0,0 +1,13 @@
+# Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i]
+# is the number of 1's in the binary representation of i.
+
+# Fib
+from typing import List
+
+
+def countBits(n: int) -> List[int]:
+    ans = [0, 1]
+    while len(ans) <= n:
+        for num in ans[:]:
+            ans.append(num + 1)
+    return ans[:n + 1]
--- tests/test_dynamic.py
+++ tests/test_dynamic.py
@@ -1,12 +1,17 @@
 import unittest
 
 from dynamic.can_sum import find_target_sum_ways
+from dynamic.counting_bit import countBits
 
 
 class DynamicTest(unittest.TestCase):
     def test_min_path_sum(self):
         result = find_target_sum_ways([1, 1, 1, 1, 1], 3)
         self.assertEqual(result, 5)
+
+    def test_countBits(self):
+        result = countBits(5)
+        self.assertEqual(result, [0, 1, 1, 2, 1, 2])
 
 
 if __name__ == '__main__':
