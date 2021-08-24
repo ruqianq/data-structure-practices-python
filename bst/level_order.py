70fa46c865b51e034053a8763db207ad3ec359d7
ruqianq <daisy@acts.net>
2021-08-24 19:35:03 -0400

    format

--- bst/level_order.py
+++ bst/level_order.py
@@ -1,4 +1,5 @@
 # Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
 from collections import deque
 from typing import Optional, List
 
