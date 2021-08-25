0227a11b9a4785d80b0c6d6d3ac2b0f18f85b8bd
ruqianq <daisy@acts.net>
2021-08-25 15:51:13 -0400

    new question

--- bst/average_of_levels.py
+++ bst/average_of_levels.py
@@ -5,4 +5,14 @@ from bst.treenode import TreeNode
 
 
 def average_of_levels(root: Optional[TreeNode]) -> List[float]:
-
+    ans, level = [], [root]
+    while level:
+        level_sum, count = 0, 0
+        temp = []
+        for node in level:
+            level_sum += node.value
+            count += 1
+            temp.extend([node.left, node.right])
+        ans.append(level_sum/count)
+        level = [leaf for leaf in temp if leaf]
+    return ans
