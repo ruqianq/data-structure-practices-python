bd08bff4b6d1741e586ee2a7631e12fb0db44526
ruqianq <daisy@acts.net>
2021-08-27 15:08:15 -0400

    new question

---
+++ bst/diameter_of_tree.py
@@ -0,0 +1,13 @@
+# Given the root of a binary tree, return the length of the diameter of the tree.
+#
+# The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
+#
+# The length of a path between two nodes is represented by the number of edges between them.
+from typing import Optional
+
+from bst.treenode import TreeNode
+
+
+def diameter_of_binary_tree(root: Optional[TreeNode]) -> int:
+
+    return
--- bst/min_distance_bw_node.py
+++ bst/min_distance_bw_node.py
@@ -1,4 +1,5 @@
-# Given the root of a Binary Search Tree (BST), return the minimum difference between the values of any two different nodes in the tree.
+# Given the root of a Binary Search Tree (BST), return the minimum difference between the values of any two different
+# nodes in the tree.
 from typing import Optional, List
 
 from bst.treenode import TreeNode
