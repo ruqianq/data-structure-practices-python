a4ec0f0582a023eb0503a830b58d8fae7e2c9fe2
Daisy Lin <dlin@captechventures.com>
2024-10-09 10:51:53 -0400

    chore: Update Python version to 3.11 and optimize segregate_evens_and_odds algorithm

--- sorting/segregate_evens_and_odds.py
+++ sorting/segregate_evens_and_odds.py
@@ -5,21 +5,20 @@ def segregate_evens_and_odds(numbers):
     There is no need to preserve the original order within the even and within the odd numbers.
     We look for a solution of the linear time complexity that uses constant auxiliary space.
     Args:
-        numbers(list_int32)
-        {
-            "numbers": [1, 2, 3, 4]
-        }
+        numbers (list_int32): List of numbers to be rearranged
     Returns:
-        list_int32
-        [4, 2, 3, 1]
+        list_int32: Rearranged list of numbers
     """
-    # Write your code here.
-    for n in numbers:
-        for i in range(n+1, len(numbers) - 1):
-            if numbers[i] % 2 == 0:
-                numbers[n], numbers[i] = numbers[i], numbers[n]
-                break
+    low = 0
+    high = len(numbers) - 1
+    
+    while low < high:
+        if numbers[low] % 2 == 0:
+            low += 1
+        else:
+            if numbers[high] % 2 == 0:
+                numbers[low], numbers[high] = numbers[high], numbers[low]
+                low += 1
+            high -= 1
+    
     return numbers
-
-
-
