88b03a024161ed1f64641b19a745f69fae748e79
Daisy Lin <daisy@acts.net>
2024-11-10 15:26:32 -0500

    pushi the code

--- permutations/get_permuations.py
+++ permutations/get_permuations.py
@@ -81,3 +81,21 @@ def get_permutations_with_set(arr):
         
     helper(arr, 0)
     return res
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        if len(nums) == 1:
+            return [nums[:]]
+
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            perms = self.permute(nums)
+
+            for perm in perms:
+                perm.append(n)
+            res.extend(perms)
+            nums.append(n)
+
+        return res 
